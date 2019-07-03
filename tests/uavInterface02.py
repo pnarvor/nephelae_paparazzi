4cc59e26df140792c78ad1dd97e05d1877c79980
Pierre Narvor <pnarvor@laas.fr>
2019-07-03 10:26:36 +0200

    [uav] Tested observer Notification

--- tests/uavInterface02.py
+++ tests/uavInterface02.py
@@ -4,13 +4,31 @@ import sys
 sys.path.append('../')
 import os
 import signal
+import time
 
 from ivy.std_api import *
 import logging
 
 import pprzinterface as ppint
 
+class Logger:
+
+    def __init__(self):
+        pass
+
+    def notify(self, sample):
+        print(sample, end="\n\n")
+
 interface = ppint.PprzInterface()
 interface.start()
 
 signal.signal(signal.SIGINT, lambda sig,fr: interface.stop())
+
+uavs = []
+while interface.running:
+    for uav in interface.uavs.keys():
+        if uav not in uavs:
+            interface.uavs[uav].sensorObservers.append(Logger())
+            uavs.append(uav)
+    time.sleep(0.5)
+
