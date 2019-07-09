6d7b6d79b2185e98d9f284760225d3678d449971
Pierre Narvor <pnarvor@laas.fr>
2019-07-09 12:34:56 +0200

    [interface] Added comments in PprzInterface

--- pprzinterface/PprzInterface.py
+++ pprzinterface/PprzInterface.py
@@ -8,15 +8,26 @@ from . import PprzUavBase
 
 class PprzInterface:
 
+    """PprzInterface
+
+    Main class to launch all uavs from. Will listen to gps messages and 
+    spawn a new PprzUavBase (or derived type) if uavId of the GPS message
+    if not already known.
+
+    A new Uav instance is built through a factory pattern 'build_uav_callback'.
+    This callback can be redefined to spawn other type of Uavs.
+    """
+
+
     def __init__(self, ivyIpp="127.255.255.255:2010",
                  build_uav_callback=lambda uavId, navRef: PprzUavBase(uavId, navRef)):
-
         self.ivyIpp   = ivyIpp
         self.navFrame = None
         self.uavs     = {}
         self.ivyBinds = []
         self.running  = False
         self.build_uav_callback = build_uav_callback
+
 
     def start(self):
 
@@ -35,24 +46,28 @@ class PprzInterface:
         self.ivyBinds.append(pmsg.Gps.bind(self.found_uav_callback))
         self.running = True
 
+
     def stop(self):
         if self.running:
             print("Shutdown... ", end="")
             for bindId in self.ivyBinds:
                 IvyUnBindMsg(bindId)
             for uav in self.uavs.values():
-                uav.terminate()
+                if uav is not None:
+                    uav.terminate()
             uavs = {}
             IvyStop()
             self.running = False
             print("Complete.")
+
 
     def found_uav_callback(self, msg):
         uavId = msg.uavId
         if not uavId in self.uavs.keys():
             self.uavs[uavId] = self.build_uav_callback(uavId, self.navFrame)
             print("Found UAV, id :", uavId)
-    
+
+
     def __getitem__(self, key):
         return self.uavs[key]
 
