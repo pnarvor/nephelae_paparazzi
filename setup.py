4402f14002d08406467495fa76885d5333a321f6
Pierre Narvor <pnarvor@laas.fr>
2019-08-23 14:45:25 +0200

    [refactor][wip] Added a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='nephelae_paparazzi',
+      version='0.1',
+      description='Paprazzi UAV utilities for nephelae_project',
+      url='ssh://git@redmine.laas.fr/laas/users/simon/nephelae/nephelae-devel/nephelae_paparazzi.git',
+      author='Pierre Narvor',
+      author_email='pnarvor@laas.fr',
+      licence='bsd3',
+      packages=find_packages(include=['nephelae_paparazzi*']),
+      install_requires=[
+        'numpy',
+        'matplotlib',
+        'ivy-python',
+        'netCDF4'
+      ],
+      zip_safe=False)
+
+
