c6503f83742d5a736df3ce18ab701617b998d4e7
Pierre Narvor <pnarvor@laas.fr>
2019-09-03 17:30:52 +0200

    Successfull lwc feedback for lace pattern

--- nephelae_paparazzi/PprzMesonhUav.py
+++ nephelae_paparazzi/PprzMesonhUav.py
@@ -2,17 +2,41 @@ import sys
 import os
 import traceback
 
-from ivy.std_api import *
-import logging
-
 from netCDF4 import MFDataset
 
-from nephelae.types  import SensorSample
-from nephelae_mesonh import MesonhVariable
-from nephelae_mesonh import MesonhCachedProbe
+from nephelae.types  import SensorSample, Bounds
+from nephelae_mesonh import MesonhVariable, MesonhCachedProbe
 
 from . import messages as pmsg
 from .PprzUavBase import PprzUavBase
+
+from ivy.std_api import *
+import logging
+
+PPRZ_HOME = os.getenv("PAPARAZZI_HOME", os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../')))
+sys.path.append(PPRZ_HOME + "/var/lib/python")
+
+# from pprzlink.ivy import IvyMessagesInterface
+from pprzlink.message import PprzMessage
+from pprzlink import messages_xml_map
+
+def pprzlink_send(msg, sender_id=None):
+    # copied from official pprzlink (to be fixed)
+    if isinstance(msg, PprzMessage):
+        if "telemetry" in msg.msg_class:
+            if sender_id is None:
+                print("ac_id needed to send telemetry message.")
+                return None
+            else:
+                return IvySendMsg("%d %s %s" % (sender_id, msg.name, msg.payload_to_ivy_string()))
+        else:
+            if sender_id is None:
+                return IvySendMsg("%s %s %s" % (msg.msg_class, msg.name, msg.payload_to_ivy_string()))
+            else:
+                return IvySendMsg("%s %s %s" % (str(sender_id), msg.name, msg.payload_to_ivy_string()))
+    else:
+        return IvySendMsg(msg)
+
 
 class PprzMesonhUav(PprzUavBase):
 
@@ -44,6 +68,9 @@ class PprzMesonhUav(PprzUavBase):
                                                  updateThreshold)
             self.probes[str(var)].start()
         self.initialized = False
+        if 'RCT' in mesonhVariables:
+            b = self.probes['RCT'].var.actual_range[0]
+            self.rctBounds = Bounds(b[0], b[-1])
         super().__init__(uavId, navFrame)
 
 
@@ -65,10 +92,16 @@ class PprzMesonhUav(PprzUavBase):
         
         for var in self.probes.keys():
             try:
+                value = self.probes[var][readKeys]
+                if var == 'RCT':
+                    msg = PprzMessage('datalink', 'PAYLOAD_COMMAND')
+                    msg['ac_id']   = int(self.id)
+                    msg['command'] = [int(255 * value / self.rctBounds.span())]
+                    pprzlink_send(msg)
                 sample = SensorSample(variableName=var, producer=self.id,
                                       timeStamp=position.t,
                                       position=position,
-                                      data=[self.probes[var][readKeys]])
+                                      data=[value])
                 self.notify_sensor_sample(sample)
             except Exception as e:
                 print(traceback.format_exc())
