20032eeae7cc96aa79099269e0fc9f216bf2d440
Pierre Narvor <pnarvor@laas.fr>
2019-10-16 12:24:12 +0200

    [pprzlink] Falling back to standalone pprzlink installation if PAPARAZZI_HOME not defined

--- nephelae_paparazzi/common.py
+++ nephelae_paparazzi/common.py
@@ -1,8 +1,17 @@
 import os
 import sys
 
-PPRZ_HOME = os.getenv("PAPARAZZI_HOME", os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../')))
-sys.path.append(PPRZ_HOME + "/var/lib/python")
+# PPRZ_HOME = os.getenv("PAPARAZZI_HOME", os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../')))
+# sys.path.append(PPRZ_HOME + "/var/lib/python")
+
+PPRZ_HOME = os.getenv("PAPARAZZI_HOME", None)
+if PPRZ_HOME is not None:
+    sys.path.append(PPRZ_HOME + "/var/lib/python")
+else:
+    PPRZLINK_HOME = os.getenv("PAPARAZZI_PPRZLINK", None)
+    if PPRZLINK_HOME is not None:
+        print(os.path.join(PPRZLINK_HOME, 'lib/v2.0/python'))
+        sys.path.append(os.path.join(PPRZLINK_HOME, 'lib/v2.0/python'))
 
 from pprzlink.ivy import IvyMessagesInterface
 # from pprzlink.message import PprzMessage
