ba3c1dfdb92f34d9fc70668e5b66968092eb0cf0
Pierre Narvor <pnarvor@laas.fr>
2019-11-05 13:56:12 +0100

    [missions/rules] Added a check for rules cosistency in ParameterRules

--- nephelae_paparazzi/missions/rules/ParameterRules.py
+++ nephelae_paparazzi/missions/rules/ParameterRules.py
@@ -1,3 +1,5 @@
+from . import SimpleBounds, AllowedValues
+
 
 class ParameterRules:
     """
@@ -17,10 +19,32 @@ class ParameterRules:
         raise a ValueError exception if parameter deemed inappropriate.
         Returns either the same parameter value or a corrected one in case
         of predefined behavior (like a default value).
-        Default behavior is returning checked value (no check)
+        Default behavior is returning parameter value (no check)
     """
 
+    def check_rules_consistency(rules):
+        """
+        Checks if rules do not contain both a SimpleBounds and a
+        AllowedValues rule.
+
+        Will raise a RuntimeError if it is the case.
+        """
+
+        if (any([isinstance(rule, SimpleBounds)  for rule in rules) and 
+            any([isinstance(rule, AllowedValues) for rule in rules)):
+            # We have both a SimpleBounds rule and a AllowedValues rule in the set
+            # This is not authorized.
+            raise RuntimeError("It is not authorized to have both a "+\
+                               "SimpleBounds and a AllowedValues rules in a "+\
+                               "set of rules")
+
+
     def __init__(self, rules=[], parameterName=''):
+        
+        # Checking if there is no big issue with rules.
+        # It will raise a RuntimeError if any issue.
+        ParameterRules.check_rules_consistency(rules)
+
         self.parameterName = parameterName
         self.rules = rules
 
@@ -41,6 +65,5 @@ class ParameterRules:
         for rule in self.rules:
             parameter = rule.check(parameter)
         return parameter
-
 
 
