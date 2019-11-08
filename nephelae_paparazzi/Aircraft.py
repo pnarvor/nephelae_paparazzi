098f5990381a6d0032dce927cbe2feafc1835773
Pierre Narvor <pnarvor@laas.fr>
2019-11-08 15:49:34 +0100

    [plugins] Modified Aircraft type to new plugin implementation

--- nephelae_paparazzi/Aircraft.py
+++ nephelae_paparazzi/Aircraft.py
@@ -3,52 +3,10 @@ import threading
 import utm
 import time
 
-from nephelae.types import MultiObserverSubject
+from nephelae.types import MultiObserverSubject, Pluginable
 
 from .common import IvySendMsg, IvyUnBindMsg
 from .messages import Gps, FlightParam, NavStatus, ApStatus, Bat, MissionStatus, Config
-
-
-class PluginableSubject(MultiObserverSubject):
-
-    """
-    PluginableSubject
-
-    Intermediate class for the Aircraft type to have traits from 
-    the MultiObserverSubject but also be able to manage plugins.
-
-    (Might be more suited to a double inheritance, check this)
-
-    Attributes
-    ----------
-    __safePluginLoad : bool
-        This is to force safe load of plugin regardless of the users choice.
-        Safe load means load will be aborted if any method name loaded by the
-        plugin superseed an attribute or a method of the base class.
-    """
-
-    def __init__(self, notificationMethods, safePluginLoad=True):
-        super().__init__(notificationMethods)
-        self.__safePluginLoad = safePluginLoad
-
-
-    def load_plugin(self, plugin, safe=True, *args, **kwargs):
-        if safe or self.__safePluginLoad:
-            # Checking if method name already exists in self attributes
-            for method in plugin.__pluginmethods__():
-                methodName = str(method).split(' ')[1].split('.')[-1]
-                if hasattr(self, methodName):
-                    raise RuntimeError(
-                        "Cannot load plugin "+str(plugin)+", method '"+\
-                         methodName+"'is already in base object"+\
-                         "Load plugin in unsafe mode to override.")
-        
-        # Adding methods from plugin to this object
-        for method in plugin.__pluginmethods__():
-            setattr(self, str(method).split(' ')[1].split('.')[-1], method,
-                    lambda self, *args, **kwargs: method(self, *args, **kwargs))
-        # Calling plugin init function
-        plugin.__plugininit__(self, *args, **kwargs)
 
 
 class AircraftStatus:
@@ -178,7 +136,7 @@ class AircraftStatus:
 
 
 
-class Aircraft(PluginableSubject):
+class Aircraft(MultiObserverSubject, Pluginable):
 
     """
     Aircraft
@@ -190,7 +148,7 @@ class Aircraft(PluginableSubject):
     """
 
     def __init__(self, uavId, navFrame):
-        super().__init__(['add_gps', 'notify_status'])
+        MultiObserverSubject.__init__(self, ['add_gps', 'notify_status'])
 
         self.id          = uavId
         self.navFrame    = navFrame
