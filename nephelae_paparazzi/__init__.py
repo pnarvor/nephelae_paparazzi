470637375c0e2633021a9bade3c04c998eef5976
Pierre Narvor <pnarvor@laas.fr>
2019-12-10 10:10:32 +0100

    [deprecation] Removed a lot of deprecated code

--- nephelae_paparazzi/__init__.py
+++ nephelae_paparazzi/__init__.py
@@ -4,18 +4,6 @@ from . import missions
 from . import utils
 from . import plugins
 
-from .MessageSynchronizer import MessageSynchronizer
-
-from .PprzInterface       import PprzInterface
-from .PprzSimulation      import PprzSimulation
-
-from .PprzUavBase         import PprzUavBase, status_to_str, print_status
-from .PprzUav             import PprzUav
-from .PprzMesonhUav       import PprzMesonhUav
-from .PprzMesonhWind      import PprzMesonhWind
-from .PprzMissionUav      import PprzMissionUav
-
-
 # v0.2
 from .AircraftLogger import AircraftLogger
 from .Aircraft       import Aircraft
