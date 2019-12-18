70001bc2755e4dd82a00eb9d8a14f07288d34899
Pierre Narvor <pnarvor@laas.fr>
2019-12-18 13:32:34 +0100

    [missions] Added mission backup file handling in MissionManager

--- nephelae_paparazzi/missions/MissionManager.py
+++ nephelae_paparazzi/missions/MissionManager.py
@@ -1,3 +1,6 @@
+import pickle
+import threading
+
 from .MissionFactory import MissionFactory
 from .types import missionTypes
 from ..common import messageInterface
@@ -44,7 +47,8 @@ class MissionManager:
                  'conflictMode' : 'error'}
                ]
     
-    def __initplugin__(self, factories=None):
+    def __initplugin__(self, factories=None, inputBackupFile = None,
+                       outputBackupFile = None):
         """
         Parameters
         ----------
@@ -56,12 +60,28 @@ class MissionManager:
         factories : dict({str:MissionFactory, ...})
             factories used to build mission instances. This parameter is
             ignored if configFile is set.
+
+        inputBackupFile : str
+            Path to file from which to read previously created missions.
+            (Used in case of a warm start configuration). Ignored if is None.
+
+        outputBackupFile : str
+            Path to file to which write created missions. To be used later
+            for a warm start (usually the same as inputBackupFile.
+            Ignored if is None.
         """
 
         self.missionFactories = factories
+        self.missions         = {}
         self.lastMissionId    = 0
         self.pendingMissions  = []
         self.currentMission   = None
+        self.lock             = threading.Lock()
+        
+        self.inputBackupFile  = inputBackupFile
+        self.outputBackupFile = outputBackupFile
+        if self.inputBackupFile is not None:
+            self.load_mission_backup_file()
 
 
     def mission_types(self):
@@ -92,16 +112,81 @@ class MissionManager:
             Parameters to be passed to the mission factory for intanciating
             this mission.
         """
-
+        
         if missionType not in self.mission_types():
             raise ValueError("Cannot create a " + missionType + " for this aircraft.")
 
-        self.pendingMissions.append(self.missionFactories[missionType].build(
-            self.lastMissionId+1, self.id, duration, **missionParameters))
+        with self.lock:
+            # Creating new mission instance
+            mission = self.missionFactories[missionType].build(
+                self.new_mission_id(), self.id, duration, **missionParameters))
+            
+            # Saving it to backup file for warm start
+            if self.outputBackupFile is not None:
+                with open(self.outputBackupFile, "ab") as f:
+                    pickle.dump({mission.missionId : mission.to_dict()}, f)
+            
+            # Keeping a copy in self.missions and registering it for validation
+            self.missions[mission.missionId] = mission
+            self.pendingMissions.append(mission.missionId)
+            
+            # At this point everything went well, keeping last generated id
+            self.lastMissionId = mission.missionId
 
-        self.lastMissionId = self.lastMissionId + 1
 
-    
+    def new_mission_id(self):
+        """
+        Returns either a new id or an error if impossible.
+        (TODO: implementation to be completed)
+        """
+        newMissionId = self.lastMissionId + 1
+        if newMissionId in self.missions.keys():
+            # This should never happen but TODO strengthen it just in case
+            raise RuntimeError("Id of newly created mission already exists.")
+        return newMissionId
+
+
+    def load_mission_backup_file(self):
+        """To be implemented"""
+
+        def reload_missions(unpickledMissions):
+            """
+            Convenience function for converting unpickled mission into mission
+            instances.
+            """
+            for missionId, params in unpickledMissions.items():
+                if self.id != params['aircraftId']:
+                    raise ValueError("Error decoding mission backup file. " +
+                        "The aircraft ids do not match (got " +
+                        str(params['aircraftId']) + " but current aircraft is " +
+                        str(self.id) + "). Did you try to load the right file ?")
+                if missionId != params['missionId']:
+                    raise ValueError("Error decoding mission backup file. " +
+                                     "Mission ids do not match.")
+                self.missions[missionId] = \
+                    self.missionFactories[params['type'].build(
+                        missionId, self.id, params['duration'], **params['params']))
+
+        # This function starts here.
+        if self.inputBackupFile is None:
+            return
+
+        with self.lock:
+            with open(self.inputBackupFile, 'rb') as f:
+                while True:
+                    # loop will exit on a EOFError, no better way with pickle
+                    try:
+                        # pickle read file chunks after chunks, read chunks
+                        # corresponding to written chunks.
+                        # (N pickle.dump = N pickle.load).
+                        reload_missions(pickle.load(f))
+                    except EOFError:
+                        # loop exit here
+                        pass
+            # New last id id the biggest one from the load missions
+            self.lastMissionId = max(self.missions.keys())
+
+
     def execute_mission(self):
         """Execute last mission in self.pendingMission
             To be removed when a real management is implemented"""
