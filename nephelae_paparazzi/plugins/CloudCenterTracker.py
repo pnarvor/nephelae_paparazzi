f82d1284b662389135304d6ac4b19f78a786960b
Florian Seguin <fseguin@laas.fr>
2020-01-07 16:44:05 +0100

    Completed start and stop of CloudCenterTracker

--- nephelae_paparazzi/plugins/CloudCenterTracker.py
+++ nephelae_paparazzi/plugins/CloudCenterTracker.py
@@ -1,18 +1,38 @@
+import threading
+
+from .WindFromStatus import wind_estimate
+
 class CloudCenterTracker:
 
     def __pluginmethods__():
         return [{'name'         : 'stop',
                  'method'       : CloudCenterTracker.stop,
                  'conflictMode' : 'prepend'},
-                 'name'         : 'start',
+                {'name'         : 'start',
                  'method'       : CloudCenterTracker.start,
-                 'conflictMode' : 'append'];
+                 'conflictMode' : 'append'},
+                {'name'         : 'cloud_center_tracker_maj',
+                 'method'       : CloudCenterTracker.cloud_center_tracker_maj,
+                 'conflictMode' : 'error'}]
 
-    def __initplugin__(self):
-        pass
+    def __initplugin__(self, mapInterface, spaceX=1000, spaceY=1000):
+        self.spaceX = spaceX
+        self.spaceY = spaceY
+        self.mapInterface = mapInterface
+        self.followedCenter = None
+        self.runThread = False
+        self.windMap = None
 
     def stop(self):
-        pass
+        self.runTracking = False
 
     def start(self):
-        pass
+        self.runTracking = True
+        cloudCenterTracker_thread = threading.Thread(
+                target=self.cloud_center_tracker_maj);
+
+    def cloud_center_tracker_maj(self):
+        while runTracking:
+            if not self.followedCenter is None:
+                pass
+
